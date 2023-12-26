2ddf86acc2774ec415499b10f82b87b2fa8de5dd
Jean Dessane <jean.dessane@gmail.com>
2023-12-26 17:41:46 +0100

    first release

--- src/main.ts
+++ src/main.ts
@@ -1,36 +1,40 @@
 import * as core from '@actions/core'
 import * as github from '@actions/github'
 
-
 export async function run(): Promise<void> {
   try {
     const mainBranch: string = core.getInput('mainBranch') ?? 'dev'
     const token: string = core.getInput('token')
 
     const octokit = github.getOctokit(token)
-    const latestRelease = await octokit.rest.repos.getLatestRelease({ ...github.context.repo,  })
+    const latestRelease = await octokit.rest.repos.getLatestRelease({
+      ...github.context.repo
+    })
 
     const pullRequests = await octokit.rest.pulls.list({
       ...github.context.repo,
       base: latestRelease.data.target_commitish,
       head: mainBranch,
-      state: 'closed',
-    });
-    const linearTickets = await Promise.all(pullRequests.data.map(async pr => {
-      const comments = await octokit.rest.issues.listComments({
-        ...github.context.repo,
-        issue_number: pr.number,
-      });
-      const linearComment = comments.data.find(c => {
-        core.debug(`Comment by ${c.performed_via_github_app}`)
-        return c.performed_via_github_app?.name === 'linear'
-      })
-      const ticket = linearComment?.body?.match(/\bRAY-\d+\b/)
-      return ticket?.[0].match
-    }).filter(Boolean))
+      state: 'closed'
+    })
+    const linearTickets = await Promise.all(
+      pullRequests.data
+        .map(async pr => {
+          const comments = await octokit.rest.issues.listComments({
+            ...github.context.repo,
+            issue_number: pr.number
+          })
+          const linearComment = comments.data.find(c => {
+            core.debug(`Comment by ${c.performed_via_github_app}`)
+            return c.performed_via_github_app?.name === 'linear'
+          })
+          const ticket = linearComment?.body?.match(/\bRAY-\d+\b/)
+          return ticket?.[0].match // eslint-disable-line @typescript-eslint/unbound-method
+        })
+        .filter(Boolean)
+    )
 
     core.debug(`Tickets found ${linearTickets.join()}`)
-
   } catch (error) {
     // Fail the workflow run if an error occurs
     if (error instanceof Error) core.setFailed(error.message)
